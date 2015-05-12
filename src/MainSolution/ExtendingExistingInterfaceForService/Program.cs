96a92ee99f1c3b5f28cae81d4a04aaed6800fb3b
Krzysztof Wasik <krzysiek.wasik.pl@gmail.com>
2015-05-12 07:47:16 +0200

    Added projects: ExtendingExistingInterfaceForServive and FactoryMethodPatternExample1

---
+++ src/MainSolution/ExtendingExistingInterfaceForService/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ExtendingExistingInterfaceForService
+{
+    /// <summary>
+    /// Interface doing something in legacy code  
+    /// </summary>
+    public interface IMyCode
+    {
+        void GetResult();
+    }
+
+    /// <summary>
+    ///  New interface which inherit from IMyCode so this interface can by expose by servie for exampel
+    /// </summary>
+    [ServiceContract]
+    public interface IMyCodeService : IMyCode
+    {
+        [OperationContract]
+        void GetResult();
+    }
+    
+      public class MyCodeService : IMyCodeService
+    {
+        public void GetResult()
+        {
+            Console.WriteLine("Result from IMyCodeService");
+        }
+    }
+      
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCodeService service = new MyCodeService();
+            IMyCodeService serviceContract = (IMyCodeService)service;
+            IMyCode codeContract = (IMyCode)service;
+
+            service.GetResult();
+            serviceContract.GetResult();
+            codeContract.GetResult();
+
+
+            // Prevents from shutdown console window
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/MainSolution/FactoryMethodPatternExample1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternExample1
+{
+    enum Animals
+    {
+        Cat, Dog, Wolf
+    }
+    
+    /// <summary>
+    /// Interface describing behaviour of target classes
+    /// </summary>
+    interface IAnimal
+    {
+        void MakeSount();
+    }
+
+    /// <summary>
+    /// Class describing cat behaviour
+    /// </summary>
+    class Cat : IAnimal
+    {
+        public void MakeSount()
+        {
+            Console.WriteLine("Miauuuuu!");
+        }
+    }
+
+    /// <summary>
+    /// Classs describing dog behaviour
+    /// </summary>
+    class Dog : IAnimal
+    {
+        public void MakeSount()
+        {
+            Console.WriteLine("Hauuuuu!");
+        }
+    }
+
+    /// <summary>
+    /// Class describing wolf behaviour
+    /// </summary>
+    class Wolf : IAnimal
+    {
+        public void MakeSount()
+        {
+            Console.WriteLine("Auuuu!");
+        }
+    }
+
+    /// <summary>
+    /// Class inmplementing factory method patter for creating instances which shares IAnimal interface
+    /// </summary>
+    static class AnimalFactory
+    {
+        public static IAnimal CreateAnimalObject(Animals animalType)
+        {
+            IAnimal animal = null;
+
+            switch (animalType)
+            {
+                case Animals.Cat:
+                    animal = new Cat();
+                    break;
+                case Animals.Dog:
+                    animal = new Dog();
+                    break;
+                case Animals.Wolf:
+                    animal = new Wolf();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("animalType","Provided animal type is not supported");
+            }
+            
+            return animal;
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal animal = AnimalFactory.CreateAnimalObject(Animals.Dog);
+            animal.MakeSount();
+                       
+            Console.ReadKey();
+        }
+    }
+}

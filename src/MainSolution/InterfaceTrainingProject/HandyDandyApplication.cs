493a440a30467ff3dd57a506286fb2e9965f7009
Krzysztof Wasik <krzysiek.wasik.pl@gmail.com>
2015-05-12 20:37:21 +0200

    Added project files with test project for unit testing

---
+++ src/MainSolution/InterfaceTrainingProject/HandyDandyApplication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using InterfaceTrainingProject.Data.SqlServer;
+using InterfaceTrainingProject.Data;
+
+namespace InterfaceTrainingProject
+{
+ 
+    public class HandyDandyApplication
+    {
+        private IUserRepository _users;
+
+        public HandyDandyApplication(IUserRepository repository = null)
+        {
+            _users = repository ?? new UserRepository();
+        }
+
+        public void DisplayUsers(TextWriter writer)
+        {
+            foreach (var user in _users.All())
+            {
+                var userData = string.Format("{0}\t{1} {2}",
+                    user.Id,
+                    user.FirstName,
+                    user.LastName);
+
+                writer.WriteLineAsync(userData);
+            }
+        }
+
+    }
+}
---
+++ src/MainSolution/InterfaceTrainingProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTrainingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new HandyDandyApplication();
+            app.DisplayUsers(Console.Out);
+
+
+            Console.ReadKey();
+        }
+    }
+}

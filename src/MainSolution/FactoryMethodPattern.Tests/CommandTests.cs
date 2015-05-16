8b86354f5f279e1c9e3359dd60934a2d10b6f6c7
Krzysztof Wasik <krzysiek.wasik.pl@gmail.com>
2015-05-16 16:32:16 +0200

    Added project for training factory method pattern

---
+++ src/MainSolution/FactoryMethodPattern.Tests/CommandTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FactoryMethodPattern.Tests
+{
+    [TestFixture]
+    public class CommandTests
+    {
+        [Test]
+        public void CommandTests_IsEqualVolmCmdWithDifferentParamFormat_ReturnsTrue()
+        {
+            var volume1 = new AquosCommand("VOLM", "30");
+            var volume2 = AquosCommand.Volume(30);
+
+            Assert.True(volume1.ToString() == volume2.ToString());
+        }
+
+        [Test]
+        public void CommandTests_IsEqualPowrCmdWithDifferentParamFormat_ReturnTrue()
+        {
+            var power1 = new AquosCommand("POWR", "0");
+            var power2 = AquosCommand.Power(PowerSetting.Off);
+
+            Assert.True(power1.ToString() == power2.ToString());
+
+           
+        }
+    }
+}
---
+++ src/MainSolution/FactoryMethodPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * FactoryMethod pattern  downside:
+ * - anyone who is consuming our API has to be aware that this methods exists 
+ * 
+ * Use factory methods:
+ * - when it is necessary to create multiple constractor override methods for creating instance of the same object
+ */
+
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}

d26d00cdf28276415b76886b2224b79e5fb9e0f4
Krzysztof Wasik <krzysiek.wasik.pl@gmail.com>
2015-05-18 22:39:54 +0200

    Add web api project for web services training

---
+++ src/MainSolution/WebService/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// Project based on Tutsplus.com lesson: 
+// https://code.tutsplus.com/courses/aspnet-at-your-service-web-api/lessons/creating-a-web-api-project
+
+namespace WebService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

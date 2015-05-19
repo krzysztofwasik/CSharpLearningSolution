01695a85452676d2340468f4afbbb18ea4649529
Krzysztof Wasik <krzysiek.wasik.pl@gmail.com>
2015-05-19 22:00:24 +0200

    Added empty Web Api project for futher work

---
+++ src/MainSolution/WebApiService/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiService.App_Start
+{
+    public static class WebApiConfig
+    {
+        public const string DEFAULT_ROUTE_NAME = "MyDefaultRoute";
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: DEFAULT_ROUTE_NAME,
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.EnableSystemDiagnosticsTracing();
+        }
+    }
+}

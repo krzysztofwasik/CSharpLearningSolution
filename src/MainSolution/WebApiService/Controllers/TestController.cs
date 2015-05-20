f9607a24e0344b41a9259ea9edc4439a7e179119
Krzysztof Wasik <krzysiek.wasik.pl@gmail.com>
2015-05-20 22:30:53 +0200

    Added based controller

---
+++ src/MainSolution/WebApiService/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiService.Controllers
+{
+    public class TestController : ApiController
+    {
+        public int Get()
+        {
+            return 1000;
+        }
+    
+}
+}
